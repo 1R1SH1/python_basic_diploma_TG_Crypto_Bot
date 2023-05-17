0e422f539d425fc9d39104ae51afa59ba04d8275
Rishat Murzyev <rmurzyev@mail.ru>
2023-05-17 19:15:13 +0300

    Completed /custom command

--- tg_API/core.py
+++ tg_API/core.py
@@ -10,53 +10,84 @@ boty = Bot(tg.tg_api_key.get_secret_value())
 
 bot = Dispatcher(boty)
 
-keyboard = KeyboardInterface.keyboarding()
+keyboard_high = KeyboardInterface.keyboarding_high()
+keyboard_low = KeyboardInterface.keyboarding_low()
+keyboard_custom = KeyboardInterface.keyboarding_custom()
 
 
 @bot.message_handler(commands=['high'])
-async def _coin_higher_price(message: types.Message):
-    await message.answer('Выберите криптовалюту, пожалуйста:', reply_markup=keyboard)
+async def _higher(message: types.Message):
+    await message.answer('Выберите криптовалюту, пожалуйста:', reply_markup=keyboard_high)
 
-    @bot.callback_query_handler(text=['Qwsogvtv82FCd', 'razxDUgYGNAdQ', 'D7B1x_ks7WhV5', 'a91GCGd_u96cF'])
-    async def _call_high(call: types.CallbackQuery):
-        if call.data == 'Qwsogvtv82FCd':
-            await call.answer('Qwsogvtv82FCd')
-        if call.data == 'razxDUgYGNAdQ':
-            await call.answer('razxDUgYGNAdQ')
-        if call.data == 'D7B1x_ks7WhV5':
-            await call.answer('D7B1x_ks7WhV5')
-        if call.data == 'a91GCGd_u96cF':
-            await call.answer('a91GCGd_u96cF')
+    @bot.callback_query_handler(text=['high_bitcoin', 'high_etherium', 'high_litecoin', 'high_dogecoin'])
+    async def _high(call: types.CallbackQuery):
+        if call.data == 'high_bitcoin':
+            cal = 'Qwsogvtv82FCd'
+        if call.data == 'high_etherium':
+            cal = 'razxDUgYGNAdQ'
+        if call.data == 'high_litecoin':
+            cal = 'D7B1x_ks7WhV5'
+        if call.data == 'high_dogecoin':
+            cal = 'a91GCGd_u96cF'
 
         coins = SiteApiInterface.get_coin()
-        response = coins(method, url, headers, call.data, 3)
+        response = coins(method, url, headers, cal, 3)
         response = response.json()
         result = max(response['data']['history'], key=lambda feature: feature['price'])
         answer = result['price']
-        await message.answer(f'Наибольшая цена за сегодня: {answer} $')
+        await call.message.answer(f'Наибольшая цена за сегодня: {answer} $')
 
 
 @bot.message_handler(commands=['low'])
-async def _coin_lower_price(message: types.Message):
-    await message.answer('Выберите криптовалюту, пожалуйста:', reply_markup=keyboard)
+async def _lower(message: types.Message):
+    await message.answer('Выберите криптовалюту, пожалуйста:', reply_markup=keyboard_low)
 
-    @bot.callback_query_handler(text=['Qwsogvtv82FCd', 'razxDUgYGNAdQ', 'D7B1x_ks7WhV5', 'a91GCGd_u96cF'])
-    async def _call_low(call: types.CallbackQuery):
-        if call.data == 'Qwsogvtv82FCd':
-            await call.answer('Qwsogvtv82FCd')
-        if call.data == 'razxDUgYGNAdQ':
-            await call.answer('razxDUgYGNAdQ')
-        if call.data == 'D7B1x_ks7WhV5':
-            await call.answer('D7B1x_ks7WhV5')
-        if call.data == 'a91GCGd_u96cF':
-            await call.answer('a91GCGd_u96cF')
+    @bot.callback_query_handler(text=['low_bitcoin', 'low_etherium', 'low_litecoin', 'low_dogecoin'])
+    async def _low(call: types.CallbackQuery):
+        if call.data == 'low_bitcoin':
+            cal = 'Qwsogvtv82FCd'
+        if call.data == 'low_etherium':
+            cal = 'razxDUgYGNAdQ'
+        if call.data == 'low_litecoin':
+            cal = 'D7B1x_ks7WhV5'
+        if call.data == 'low_dogecoin':
+            cal = 'a91GCGd_u96cF'
 
         coins = SiteApiInterface.get_coin()
-        response = coins(method, url, headers, call.data, 3)
+        response = coins(method, url, headers, cal, 3)
         response = response.json()
         result = min(response['data']['history'], key=lambda feature: feature['price'])
         answer = result['price']
-        await message.answer(f'Наименьшая цена за сегодня: {answer} $')
+        await call.message.answer(f'Наименьшая цена за сегодня: {answer} $')
+
+
+@bot.message_handler(commands=['custom'])
+async def _custom(message: types.Message):
+    await message.answer('Выберите криптовалюту, пожалуйста:', reply_markup=keyboard_custom)
+
+    @bot.callback_query_handler(text=['avr_bitcoin', 'avr_etherium', 'avr_litecoin', 'avr_dogecoin'])
+    async def _avr(call: types.CallbackQuery):
+        if call.data == 'avr_bitcoin':
+            cal = 'Qwsogvtv82FCd'
+        if call.data == 'avr_etherium':
+            cal = 'razxDUgYGNAdQ'
+        if call.data == 'avr_litecoin':
+            cal = 'D7B1x_ks7WhV5'
+        if call.data == 'avr_dogecoin':
+            cal = 'a91GCGd_u96cF'
+
+        coins = SiteApiInterface.get_coin()
+        response = coins(method, url, headers, cal, 3)
+        response = response.json()
+        result_min = min(response['data']['history'], key=lambda feature: feature['price'])
+        result_max = max(response['data']['history'], key=lambda feature: feature['price'])
+        price_min = result_min['price']
+        price_max = result_max['price']
+        to_float_min = float(price_min)
+        to_float_max = float(price_max)
+        prices = [to_float_min, to_float_max]
+        avr = sum(prices) / 2
+        await call.message.answer(f'Средняя цена за сегодня: {avr} $')
 
 
 @bot.message_handler(commands=['hello-world'])
@@ -73,12 +104,16 @@ async def _greetings(message: types.Message):
 class TGCoreInterface():
 
     @staticmethod
-    def coin_lower_price():
-        return _coin_lower_price
+    def lower():
+        return _lower
 
     @staticmethod
-    def coin_higher_price():
-        return _coin_higher_price
+    def higher():
+        return _higher
+
+    @staticmethod
+    def custom():
+        return _custom
 
     @staticmethod
     def get_start():
@@ -94,7 +129,8 @@ class TGCoreInterface():
 if __name__ == '__main__':
     _start()
     _greetings()
-    _coin_lower_price()
-    _coin_higher_price()
+    _lower()
+    _higher()
+    _custom
 
     TGCoreInterface()
--- tg_API/keyboard/keyboard_core.py
+++ tg_API/keyboard/keyboard_core.py
@@ -1,25 +1,58 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
 
-button1 = InlineKeyboardButton(text='Bitcoin', callback_data='Qwsogvtv82FCd')
-button2 = InlineKeyboardButton(text='Ethereum', callback_data='razxDUgYGNAdQ')
-button3 = InlineKeyboardButton(text='Litecoin', callback_data='D7B1x_ks7WhV5')
-button4 = InlineKeyboardButton(text='Dogecoin', callback_data='a91GCGd_u96cF')
+button_low_1 = InlineKeyboardButton(text='Bitcoin', callback_data='low_bitcoin')
+button_low_2 = InlineKeyboardButton(text='Ethereum', callback_data='low_etherium')
+button_low_3 = InlineKeyboardButton(text='Litecoin', callback_data='low_litecoin')
+button_low_4 = InlineKeyboardButton(text='Dogecoin', callback_data='low_dogecoin')
+
+button_high_1 = InlineKeyboardButton(text='Bitcoin', callback_data='high_bitcoin')
+button_high_2 = InlineKeyboardButton(text='Ethereum', callback_data='high_etherium')
+button_high_3 = InlineKeyboardButton(text='Litecoin', callback_data='high_litecoin')
+button_high_4 = InlineKeyboardButton(text='Dogecoin', callback_data='high_dogecoin')
+
+button_custom_1 = InlineKeyboardButton(text='Bitcoin', callback_data='avr_bitcoin')
+button_custom_2 = InlineKeyboardButton(text='Ethereum', callback_data='avr_etherium')
+button_custom_3 = InlineKeyboardButton(text='Litecoin', callback_data='avr_litecoin')
+button_custom_4 = InlineKeyboardButton(text='Dogecoin', callback_data='avr_dogecoin')
 
 
-def _keyboarding():
-    keyboard_inline = InlineKeyboardMarkup().add(button1, button2, button3, button4)
+def _keyboarding_low():
+    keyboard_inline_low = InlineKeyboardMarkup().add(button_low_1, button_low_2, button_low_3, button_low_4)
 
-    return keyboard_inline
+    return keyboard_inline_low
+
+
+def _keyboarding_high():
+    keyboard_inline_high = InlineKeyboardMarkup().add(button_high_1, button_high_2, button_high_3, button_high_4)
+
+    return keyboard_inline_high
+
+
+def _keyboarding_custom():
+    keyboard_inline_custom = InlineKeyboardMarkup().add(button_custom_1, button_custom_2, button_custom_3,
+                                                        button_custom_4)
+
+    return keyboard_inline_custom
 
 
 class KeyboardInterface():
 
     @staticmethod
-    def keyboarding():
-        return _keyboarding()
+    def keyboarding_low():
+        return _keyboarding_low()
+
+    @staticmethod
+    def keyboarding_high():
+        return _keyboarding_high()
+
+    @staticmethod
+    def keyboarding_custom():
+        return _keyboarding_custom()
 
 
 if __name__ == '__main__':
-    _keyboarding()
+    _keyboarding_low()
+    _keyboarding_high()
+    _keyboarding_custom()
 
     KeyboardInterface()
